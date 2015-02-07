684af149116b58ad8ca47761bdd29c56765242eb
cantbelikewater <cantbelikewater@gmail.com>
2015-02-07 15:26:36 -0800

    first commit

---
+++ android/src/com/cantbelikewater/interstellar/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.cantbelikewater.interstellar.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.cantbelikewater.interstellar.Interstellar;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Interstellar(), config);
+	}
+}
---
+++ desktop/src/com/cantbelikewater/interstellar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cantbelikewater.interstellar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cantbelikewater.interstellar.Interstellar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Interstellar(), config);
+	}
+}
